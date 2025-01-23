78797a8fb553586b95aa57ac3a3fe49678da56e8
Zuhii mz <165068338+ZuhiiMalakShah@users.noreply.github.com>
2025-01-23 13:22:47 +0500

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,7 +14,7 @@ function Footer() {
               </div>
               <div>
                 <p className="text-sm text-gray-600">
-                  &copy; Copyright 2025. All Rights Reserved by Asif Iqbal Khan.
+                  &copy; Copyright 2025. All Rights Reserved by Aliya Manzoor .
                 </p>
               </div>
             </div>
